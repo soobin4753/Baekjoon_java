33b3f311dd8d9d15d5727ef3ff53951dc12b3eda
soobin4753 <soobin4753@naver.com>
2022-11-03 17:09:11 +0900

    commit

---
+++ src/step3/Q5.java
@@ -0,0 +1,21 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+	// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i = 1; i <= a; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
